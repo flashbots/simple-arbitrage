4857d85aba518ade02221b5362b6f499c568b910
Robert Miller <bert.c.miller@gmail.com>
2021-05-06 08:00:17 -0400

    Add flashloan file

--- contracts/BundleExecutor.sol
+++ contracts/BundleExecutor.sol
@@ -1,35 +1,22 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.6.12;
-
 pragma experimental ABIEncoderV2;
 
-interface IERC20 {
-    event Approval(address indexed owner, address indexed spender, uint value);
-    event Transfer(address indexed from, address indexed to, uint value);
-
-    function name() external view returns (string memory);
-    function symbol() external view returns (string memory);
-    function decimals() external view returns (uint8);
-    function totalSupply() external view returns (uint);
-    function balanceOf(address owner) external view returns (uint);
-    function allowance(address owner, address spender) external view returns (uint);
-
-    function approve(address spender, uint value) external returns (bool);
-    function transfer(address to, uint value) external returns (bool);
-    function transferFrom(address from, address to, uint value) external returns (bool);
-}
+import "./FlashLoanReceiverBase.sol";
+import "./Interfaces.sol";
+import "./Libraries.sol";
 
 interface IWETH is IERC20 {
     function deposit() external payable;
     function withdraw(uint) external;
 }
 
-// This contract simply calls multiple targets sequentially, ensuring WETH balance before and after
-
-contract FlashBotsMultiCall {
+contract FlashBotsMultiCallFL is FlashLoanReceiverBase {
+    using SafeMath for uint256;
     address private immutable owner;
     address private immutable executor;
-    IWETH private constant WETH = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    address public WETH_address = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IWETH private constant WETH = IWETH(WETH_address);
 
     modifier onlyExecutor() {
         require(msg.sender == executor);
@@ -41,7 +28,7 @@ contract FlashBotsMultiCall {
         _;
     }
 
-    constructor(address _executor) public payable {
+    constructor(address _executor, ILendingPoolAddressesProvider _addressProvider) FlashLoanReceiverBase(_addressProvider) public payable {
         owner = msg.sender;
         executor = _executor;
         if (msg.value > 0) {
@@ -49,27 +36,72 @@ contract FlashBotsMultiCall {
         }
     }
 
-    receive() external payable {
+    /**
+        This function is called after your contract has received the flash loaned amount
+     */
+    function executeOperation(
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata premiums,
+        address initiator,
+        bytes calldata params
+    )
+        external
+        override
+        returns (bool)
+    {
+        uint amountOwing = amounts[0].add(premiums[0]);
+        uniswapWethFLParams(amounts[0], params, amountOwing);
+        WETH.approve(address(LENDING_POOL), amountOwing);
+
+        return true;
     }
 
-    function uniswapWeth(uint256 _wethAmountToFirstMarket, uint256 _ethAmountToCoinbase, address[] memory _targets, bytes[] memory _payloads) external onlyExecutor payable {
-        require (_targets.length == _payloads.length);
-        uint256 _wethBalanceBefore = WETH.balanceOf(address(this));
-        WETH.transfer(_targets[0], _wethAmountToFirstMarket);
+    function flashloan(uint256 amountToBorrow, bytes memory _params) external onlyExecutor() {
+        address receiverAddress = address(this);
+
+        address[] memory assets = new address[](1);
+        assets[0] = WETH_address;
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = amountToBorrow;
+
+        uint256[] memory modes = new uint256[](1);
+        modes[0] = 0;
+
+        address onBehalfOf = address(this);
+        uint16 referralCode = 161;
+
+        LENDING_POOL.flashLoan(
+            receiverAddress,
+            assets,
+            amounts,
+            modes,
+            onBehalfOf,
+            _params,
+            referralCode
+        );
+    }
+
+    function uniswapWethFLParams(uint256 _amountToFirstMarket, bytes memory _params, uint256 totalAaveDebt) internal {
+        (uint256 _ethAmountToCoinbase, address[] memory _targets, bytes[] memory _payloads) = abi.decode(_params, (uint256, address[], bytes[]));
+        require(_targets.length == _payloads.length);
+
+        WETH.transfer(_targets[0], _amountToFirstMarket);
         for (uint256 i = 0; i < _targets.length; i++) {
             (bool _success, bytes memory _response) = _targets[i].call(_payloads[i]);
-            require(_success); _response;
+            require(_success); 
         }
 
         uint256 _wethBalanceAfter = WETH.balanceOf(address(this));
-        require(_wethBalanceAfter > _wethBalanceBefore + _ethAmountToCoinbase);
-        if (_ethAmountToCoinbase == 0) return;
 
-        uint256 _ethBalance = address(this).balance;
-        if (_ethBalance < _ethAmountToCoinbase) {
-            WETH.withdraw(_ethAmountToCoinbase - _ethBalance);
-        }
+        uint256 _profit = _wethBalanceAfter - totalAaveDebt - _ethAmountToCoinbase;
+        
+        require(_profit > 0);
+
+        WETH.withdraw(_ethAmountToCoinbase + _profit);
         block.coinbase.transfer(_ethAmountToCoinbase);
+        msg.sender.transfer(_profit);
     }
 
     function call(address payable _to, uint256 _value, bytes calldata _data) external onlyOwner payable returns (bytes memory) {
@@ -77,5 +109,8 @@ contract FlashBotsMultiCall {
         (bool _success, bytes memory _result) = _to.call{value: _value}(_data);
         require(_success);
         return _result;
+    }
+
+    receive() external payable {
     }
 }
